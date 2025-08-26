6872baa8dcfe4dc76efd653d1771af7f3437a688
Luan Lima <lluan.lima94@gmail.com>
2025-08-26 19:57:35 -0300

    refactor: streamline component code and enhance message handling

--- src/app/messages/messages.service.ts
+++ src/app/messages/messages.service.ts
@@ -1,13 +1,37 @@
 import { Injectable, signal } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MessagesService {
   private messages = signal<string[]>([]);
+
   allMessages = this.messages.asReadonly();
 
   addMessage(message: string) {
-    this.messages.update((prevMessages) => [...prevMessages, message]);
+    this.messages.update((oldMessages) => [...oldMessages, message]);
   }
 }
+// import { Injectable, signal } from '@angular/core';
+// import { BehaviorSubject } from 'rxjs';
+
+// @Injectable({
+//   providedIn: 'root',
+// })
+// export class MessagesService {
+//   // private messages = signal<string[]>([]);
+//   private messages: string[] = [];
+//   messages$ = new BehaviorSubject<string[]>([]);
+
+//   // allMessages = this.messages.asReadonly();
+
+//   get allMessages() {
+//     return [...this.messages];
+//   }
+
+//   addMessage(message: string) {
+//     this.messages = [...this.messages, message];
+//     this.messages$.next(this.messages);
+//   }
+// }
